9a804fc4efc0da70dfd5479a6851e8ebd3159f19
Fernandes <bnfs@gft.com>
2024-02-27 15:48:05 -0300

    Project created

---
+++ demo-azure-storage/src/main/java/com/fernantech/demoazurestorage/DemoAzureStorageApplication.java
@@ -0,0 +1,13 @@
+package com.fernantech.demoazurestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAzureStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAzureStorageApplication.class, args);
+	}
+
+}
